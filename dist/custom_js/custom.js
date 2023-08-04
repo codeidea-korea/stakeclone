df210e8bd890e35db771cbfdde1b2ae9b3e5df2a
yooonmi12 <122426957+yooonmi12@users.noreply.github.com>
2023-08-04 17:58:26 +0900

    모바일, 우측 컨텐츠 완료

--- dist/custom_js/custom.js
+++ dist/custom_js/custom.js
@@ -33,6 +33,7 @@ fetch("/stakeclone/_top_bar.html")
         $(".content").prepend(html);
 
         topbarHandle();
+        customJquery();
     })
     .catch((error) => {
         console.log(error);
@@ -43,6 +44,8 @@ fetch("/stakeclone/_right_bar.html")
     .then((response) => response.text())
     .then((html) => {
         $(".right-sidebar").html(html);
+        customJquery();
+        leftMenuHandle();
     })
     .catch((error) => {
         console.log(error);
@@ -144,6 +147,11 @@ const leftMenuHandle = () => {
     // };
 
     // window.addEventListener("load", sidenavFold);
+
+    // 모바일 퀵
+    $(".mo_quick li").on("click", function () {
+        $(this).addClass("on").siblings().removeClass("on");
+    });
 };
 
 //=======================================================
@@ -215,10 +223,35 @@ const topbarHandle = () => {
 //=======================================================
 //   화면 줄어들면 메뉴 접기
 //=======================================================
+let right_on = "";
+const rightClick = (item) => {
+    if (item == "close" || right_on == item) {
+        $(".right-sidebar").removeClass("on");
+        $(".content").removeClass("right_open");
+        setTimeout(() => {
+            $(`.right-sidebar > div`).hide();
+        }, 400);
+        right_on = "";
+    } else {
+        if (item == "menu_right") {
+            $(".right-sidebar").addClass("on mo_he_view");
+        } else {
+            $(".right-sidebar").addClass("on");
+            $(".right-sidebar").removeClass("mo_he_view");
+        }
+        $(".content").addClass("right_open");
+        $(`.right-sidebar > div`).hide();
+        $(`.right-sidebar .${item}`).show();
+        right_on = item;
+    }
 
-const rightClick = () => {
-    const rightSidebar = document.querySelector(".right-sidebar");
-    rightSidebar.classList.toggle("on");
+    if (item == "chatting_right" || item == "batting_right") {
+        setTimeout(function () {
+            var el = document.querySelector(".chattingDropdown");
+            var dropdown = tailwind.Dropdown.getOrCreateInstance(el);
+            dropdown.hide();
+        }, 100);
+    }
 };
 
 //=======================================================
@@ -272,88 +305,121 @@ window.addEventListener("load", function () {
 //=======================================================
 //   커스텀 js
 //=======================================================
-// 검색 커스텀
-$(".container_wrap .search_form input").on("focus", function () {
-    $(".container_wrap .search_wrap .search_modal_bg").show();
-    $(".container_wrap .search_wrap .no_search_box").show();
-    $(".container_wrap .search_wrap .search_close").show();
-});
-$(".container_wrap .search_wrap .search_modal_bg , .container_wrap .search_wrap .search_close").on("click", function () {
-    $(".container_wrap .search_wrap .no_search_box").hide();
-    $(".container_wrap .search_wrap .result_search_box").hide();
-    $(".container_wrap .search_wrap .search_modal_bg").hide();
-    $(".container_wrap .search_wrap .search_close").hide();
-});
-
-$(".container_wrap .search_form input").on("keyup", function (e) {
-    if (e.target.value.length > 0) {
-        $(".container_wrap .search_wrap .result_search_box").show();
-    } else {
+const customJquery = () => {
+    // 검색 커스텀
+    $(".container_wrap .search_form input").on("focus", function () {
+        $(".container_wrap .search_wrap .search_modal_bg").show();
+        $(".container_wrap .search_wrap .no_search_box").show();
+        $(".container_wrap .search_wrap .search_close").show();
+    });
+    $(".container_wrap .search_wrap .search_modal_bg , .container_wrap .search_wrap .search_close").on("click", function () {
+        $(".container_wrap .search_wrap .no_search_box").hide();
         $(".container_wrap .search_wrap .result_search_box").hide();
-    }
-});
+        $(".container_wrap .search_wrap .search_modal_bg").hide();
+        $(".container_wrap .search_wrap .search_close").hide();
+    });
 
-// select 커스텀
-// option_box toggle
-$(".select_custom .select_toggle_btn").on("click", function (e) {
-    $(".select_custom .option_box").hide();
-    $(".select_custom .select_toggle_btn").removeClass("active");
+    $(".container_wrap .search_form input").on("keyup", function (e) {
+        if (e.target.value.length > 0) {
+            $(".container_wrap .search_wrap .result_search_box").show();
+        } else {
+            $(".container_wrap .search_wrap .result_search_box").hide();
+        }
+    });
 
-    $(this).next(".option_box").toggle();
-    $(this).toggleClass("active");
-});
+    $(".menu_right .search_form input").on("focus", function () {
+        $(".menu_right .search_wrap .search_modal_bg").show();
+        $(".menu_right .search_wrap .no_search_box").show();
+        $(".menu_right .search_wrap .search_close").show();
+    });
+    $(".menu_right .search_wrap .search_modal_bg , .menu_right .search_wrap .search_close").on("click", function () {
+        $(".menu_right .search_wrap .no_search_box").hide();
+        $(".menu_right .search_wrap .result_search_box").hide();
+        $(".menu_right .search_wrap .search_modal_bg").hide();
+        $(".menu_right .search_wrap .search_close").hide();
+    });
 
-// 바깥 클릭했을때 option 닫기
-$("body").on("click", function (e) {
-    if (!$(e.target).closest(".select_custom").length) {
+    $(".menu_right .search_form input").on("keyup", function (e) {
+        if (e.target.value.length > 0) {
+            $(".menu_right .search_wrap .result_search_box").show();
+        } else {
+            $(".menu_right .search_wrap .result_search_box").hide();
+        }
+    });
+
+    // select 커스텀
+    // option_box toggle
+    $(".select_custom .select_toggle_btn").on("click", function (e) {
         $(".select_custom .option_box").hide();
         $(".select_custom .select_toggle_btn").removeClass("active");
-    }
-});
 
-// option 클릭했을때 값 변경, option_box 닫기
-$(".select_custom .click_option.option_box button").on("click", function () {
-    const value = $(this).find("span").text();
-    $(this).parents(".select_custom").find(".select_toggle_btn span").text(value);
-    $(this).addClass("active").siblings().removeClass("active");
-    $(this).parents(".option_box").hide();
-    $(this).parents(".select_custom").find(".select_toggle_btn").removeClass("active");
-});
+        $(this).next(".option_box").toggle();
+        $(this).toggleClass("active");
+    });
 
-// input check 했을 때,
-$(".select_custom .input_option.option_box input").on("change", function () {
-    if ($(this).prop("checked")) {
-        $(this).parents("button").addClass("active");
-        $(this).parents("button").siblings().addClass("off");
-    } else {
-        $(this).parents("button").removeClass("active");
-    }
+    // 바깥 클릭했을때 option 닫기
+    $("body").on("click", function (e) {
+        if (!$(e.target).closest(".select_custom").length) {
+            $(".select_custom .option_box").hide();
+            $(".select_custom .select_toggle_btn").removeClass("active");
+        }
+    });
 
-    let checked = 0;
-    $(this)
-        .parents(".option_box")
-        .find("button")
-        .each(function (index, item) {
-            if ($(item).find("input").prop("checked")) {
-                checked += 1;
-            }
-        });
-    if (checked > 0) {
-        $(this).parents(".select_custom").find(".select_toggle_btn span i").addClass("on").text(checked);
-    } else {
-        $(this).parents(".select_custom").find(".select_toggle_btn span i").removeClass("on");
-    }
-});
+    // option 클릭했을때 값 변경, option_box 닫기
+    $(".select_custom .click_option.option_box button").on("click", function () {
+        const value = $(this).find("span").text();
 
-$(".select_custom .input_option.option_box .clear_btn").on("click", function () {
-    $(this).parents(".select_custom").find(".select_toggle_btn").removeClass("active");
-    $(this).parents(".select_custom").find(".select_toggle_btn span i").removeClass("on").text("");
-    $(this).parents(".select_custom").find(".option_box").hide();
-    $(this)
-        .parents(".option_box")
-        .find("button")
-        .each(function (index, item) {
-            $(item).removeClass("active off");
-            $(item).find("input").prop("checked", false);
-        });
-});
+        if ($(this).parents(".option_box").hasClass("language_option")) {
+            // 채팅 > 언어 클릭시
+            const img = $(this).find("i").html();
+            $(this)
+                .parents(".select_custom")
+                .find(".select_toggle_btn span")
+                .html(img + "Stake: " + value);
+        } else {
+            $(this).parents(".select_custom").find(".select_toggle_btn span").text(value);
+        }
+
+        $(this).addClass("active").siblings().removeClass("active");
+        $(this).parents(".option_box").hide();
+        $(this).parents(".select_custom").find(".select_toggle_btn").removeClass("active");
+    });
+
+    // input check 했을 때,
+    $(".select_custom .input_option.option_box input").on("change", function () {
+        if ($(this).prop("checked")) {
+            $(this).parents("button").addClass("active");
+            $(this).parents("button").siblings().addClass("off");
+        } else {
+            $(this).parents("button").removeClass("active");
+        }
+
+        let checked = 0;
+        $(this)
+            .parents(".option_box")
+            .find("button")
+            .each(function (index, item) {
+                if ($(item).find("input").prop("checked")) {
+                    checked += 1;
+                }
+            });
+        if (checked > 0) {
+            $(this).parents(".select_custom").find(".select_toggle_btn span i").addClass("on").text(checked);
+        } else {
+            $(this).parents(".select_custom").find(".select_toggle_btn span i").removeClass("on");
+        }
+    });
+
+    $(".select_custom .input_option.option_box .clear_btn").on("click", function () {
+        $(this).parents(".select_custom").find(".select_toggle_btn").removeClass("active");
+        $(this).parents(".select_custom").find(".select_toggle_btn span i").removeClass("on").text("");
+        $(this).parents(".select_custom").find(".option_box").hide();
+        $(this)
+            .parents(".option_box")
+            .find("button")
+            .each(function (index, item) {
+                $(item).removeClass("active off");
+                $(item).find("input").prop("checked", false);
+            });
+    });
+};
